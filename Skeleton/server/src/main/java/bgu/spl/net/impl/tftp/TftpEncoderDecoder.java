af495d34b17d46bd077184d62721117681ab4ecb
yuvalku <yuvalkutai@gmail.com>
2024-03-05 13:18:41 +0200

    5.3
    Co-authored-by: OmriAmit13 <OmriAmit13@users.noreply.github.com>

--- Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -7,27 +7,102 @@ public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
 
     private byte[] bytes = new byte[1<<10];
     private int len = 0;
+    private short opCode;
+    private short dataSize;
 
     @Override
     public byte[] decodeNextByte(byte nextByte) {
 
-        if (nextByte != 0 || len == 0){
+        // Extract the opCode
+        if (len == 0){
             pushByte(nextByte);
             return null;
         }
+        else if (len == 1){
+            pushByte(nextByte);
+            opCode = (short)(((short)bytes[0]) << 8 | (short)(bytes[1]));
+            
+            if (opCode == 6 | opCode == 10){
+                return createOutput();
+            }
+            return null;
+        }
 
-        // meaning that we finish reading the message
-        return createOutput();
-        
+        // RRQ WRQ LOGRQ DELRQ
+        else if ((opCode == 1 | opCode == 2 | opCode == 7 | opCode == 8)){
+            if (nextByte != 0) {
+                pushByte(nextByte);
+                return null;
+            }
+            return createOutput();
+        }
+
+        // ACK
+        else if (opCode == 4){
+            pushByte(nextByte);
+            if (len == 4)
+                return createOutput();
+            return null;
+        }
+
+        // BCAST
+        else if (opCode == 9){
+            if (len == 2)
+                pushByte(nextByte);
+            else if (nextByte != 0){
+                pushByte(nextByte);
+                return null;
+            }
+            return createOutput();
+        }
+
+        // DATA
+        else if (opCode == 3){
+            if (len < 6)
+                pushByte(nextByte);
+            else if (len == 6){
+                dataSize = (short)(((short)bytes[2]) << 8 | (short)(bytes[3]));
+                if (dataSize != 0){
+                    pushByte(nextByte);
+                    dataSize--;
+                }
+                if (dataSize == 0)
+                    return createOutput();
+            }
+            else{
+                pushByte(nextByte);
+                dataSize--;
+                if (dataSize == 0) //need to change to 6 if the size includes the first 6 bytes
+                    return createOutput();
+            }
+            return null;
+        }
+
+        // ERROR
+        else {
+            if (len < 4)
+                pushByte(nextByte);
+            else if (nextByte != 0){
+                pushByte(nextByte);
+                return null;
+            }
+            return createOutput();
+        }
     }
 
     @Override
     public byte[] encode(byte[] message) {
-        byte[] output = new byte[message.length + 1];
-        output[output.length - 1] = 0;
-        for (int i = 0; i < output.length - 1; i++)
-            output[i] = message[i];
-        return output;
+
+        short _opCode = (short)(((short)message[0]) << 8 | (short)(message[1]));
+        if (_opCode == 7 | _opCode == 8 | _opCode == 1 | _opCode == 2 | opCode == 9 | _opCode == 5) {
+            byte[] output = new byte[message.length + 1];
+            output[output.length - 1] = 0;
+            for (int i = 0; i < output.length - 1; i++)
+                output[i] = message[i];
+            return output;
+        }
+        else 
+            return message; // maybe need to create a copy
     }
 
     // Added
